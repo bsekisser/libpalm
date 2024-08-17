2a23c8fc09c4a92e636b6d9ce97a60b4eff6ffbd
bsekisser <squirmyworms@embarqmail.com>
2024-08-16 23:15:54 -0400

    form: some fixes and beginning of loading objects
    	modified:   source/FrmInitForm.c

--- source/FrmInitForm.c
+++ source/FrmInitForm.c
@@ -15,19 +15,24 @@
 
 /* **** */
 
+#include <endian.h>
 #include <stdlib.h>
+#include <string.h>
 
 /* **** */
 
-#define LOGx(_type, _what) LOG(" %s : " _type, #_what, _what)
+#define LOGx(_f, _what, ...) LOG(" %s : " _f, #_what, _what, ##__VA_ARGS__)
 
 /* **** */
 
 FormType* FrmInitForm(UInt16 rscID)
 {
-	FormType* f = calloc(1, sizeof(FormType));
-	WindowType* fw = &f->window;
+	FormType f;
+	WindowType* fw = &f.window;
+	RectanglePtr bounds = &fw->windowBounds;
 
+	f.handler = 0;
+	f.focus = 0;
 	fw->windowFlags.dialog = 1;
 
 	MemHandle h2fr = DmGetResource(formRscType, rscID);
@@ -37,46 +42,69 @@ FormType* FrmInitForm(UInt16 rscID)
 
 	/* **** */
 
-	p = lds16be(&fw->windowBounds.topLeft.x, p);
-	p = lds16be(&fw->windowBounds.topLeft.y, p);
-	p = lds16be(&fw->windowBounds.extent.x, p);
-	p = lds16be(&fw->windowBounds.extent.y, p);
-	f->attr.usable = uint8(&p); p++;
-	fw->windowFlags.modal = uint8(&p); p++;
-	f->attr.saveBehind = uint8(&p); p++;
+	p = lds16be(&bounds->topLeft.x, p);
+	p = lds16be(&bounds->topLeft.y, p);
+	p = lds16be(&bounds->extent.x, p);
+	p = lds16be(&bounds->extent.y, p);
+
+	f.attr.usable = uint8(&p); p += 1;
+	fw->windowFlags.modal = uint8(&p); p += 1;
+	f.attr.saveBehind = uint8(&p); p += 1;
+
 	p += 2;	p += 2;
-	p = ldu16be(&f->formId, p);
-	p = ldu16be(&f->helpRscId, p);
-	p = ldu16be(&f->menuRscId, p);
-	p = ldu16be(&f->defaultButton, p);
+
+	p = ldu16be(&f.formId, p);
+	p = ldu16be(&f.helpRscId, p);
+	p = ldu16be(&f.menuRscId, p);
+	p = ldu16be(&f.defaultButton, p);
+
 	p += 2; p += 2;
 
 	if(1) {
-		LOGx("%u", fw->windowBounds.topLeft.x);
-		LOGx("%u", fw->windowBounds.topLeft.y);
-		LOGx("%u", fw->windowBounds.extent.x);
-		LOGx("%u", fw->windowBounds.extent.y);
-		LOGx("%u", f->formId);
-		LOGx("%u", f->helpRscId);
-		LOGx("%u", f->menuRscId);
-		LOGx("%u", f->defaultButton);
+		LOGx("%u", bounds->topLeft.x);
+		LOGx("%u", bounds->topLeft.y);
+		LOGx("%u", bounds->extent.x);
+		LOGx("%u", bounds->extent.y);
+		LOGx("%u", f.formId);
+		LOGx("%u", f.helpRscId);
+		LOGx("%u", f.menuRscId);
+		LOGx("%u", f.defaultButton);
 	}
 
-	unsigned itemCount = uint16be(&p);
-	LOGx("%u", itemCount);
+	p = ldu16be(&f.numObjects, p);
+	if(1)
+		LOGx("%u", f.numObjects);
 
-	for(unsigned i = 0; i < itemCount; i++)
+	const size_t objPtrAlloc = sizeof(f.objects) * f.numObjects;
+	const size_t formAlloc = sizeof(FormType) + objPtrAlloc;
+
+	FormPtr formP = calloc(1, formAlloc);
+	memcpy(formP, &f, formAlloc);
+
+	void* objectP = &formP->objects;
+
+	for(unsigned i = 0; i < f.numObjects; i++)
 	{
 		uint16_t objID = uint16be(&p);
-		uint32_t objType = uint32le(&p);
+		uint32_t objType[2] = { htole32(uint32be(&p)), 0 };
+//		uint32_t objType[2] = { uint32le(&p), 0 };
 
-		if(1) {
-			LOGx("%u", objID);
-			LOGx("%u", objType);
+		switch(objType[0]) {
+			case 'tFBM':
+//				*objectP++ = load_form_bitmap(objID, objType[0]);
+				break;
+			default:
+				LOGx("%u", objID);
+				LOGx("0x%08x (%s)", objType[0], (char*)&objType[0]);
+				LOG_ACTION(exit(-1));
+				break;
 		}
 	}
 
 	/* **** */
 
 	MemHandleUnlock(h2fr);
+	MemHandleFree(h2fr);
+
+	return(formP);
 }
