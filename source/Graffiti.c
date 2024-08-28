400d83c1464986a27f86235075af159974ef2eb8
bsekisser <squirmyworms@embarqmail.com>
2024-08-27 22:37:42 -0400

    consolidate graffiti

---
+++ source/Graffiti.c
@@ -0,0 +1,18 @@
+#include "Graffiti.h"
+
+/* **** */
+
+#include "git/libbse/include/log.h"
+#include "git/libbse/include/unused.h"
+
+/* **** */
+
+Err GrfInitState(void)
+{ LOG("TODO"); return(0); }
+
+Err GrfSetState(Boolean capsLock, Boolean numLock, Boolean upperShift)
+{
+	LOG("TODO"); return(0);
+
+	UNUSED(capsLock, numLock, upperShift);
+}
