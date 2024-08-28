a683650c8beac46549419d6985b79918e3c370e4
bsekisser <squirmyworms@embarqmail.com>
2024-08-27 22:00:48 -0400

    consolidate font manager

---
+++ source/Font.c
@@ -0,0 +1,41 @@
+#include "xFont.h"
+
+/* **** */
+
+#include "libbse/include/log.h"
+#include "libbse/include/unused.h"
+
+/* **** */
+
+#include <string.h>
+
+/* **** */
+
+void FntCharsInWidth(Char const *string,
+	Int16 *const stringWidthP, Int16 *const stringLengthP,
+	Boolean *const fitWithinWidth)
+{
+	unsigned len = strlen(string);
+
+	do { // strip trailing spaces and tabs
+		const Char c = string[len];
+		switch(c) {
+			case ' ':
+			case '\t':
+				len--;
+			default:
+				break;
+		}
+	}while(string[len] && len);
+
+	LOG("TODO"); return;
+
+	UNUSED(stringWidthP, stringLengthP, fitWithinWidth);
+}
+
+FontID FntSetFont(FontID font)
+{
+	LOG("TODO"); return(0);
+
+	UNUSED(font);
+}
