17ff08efde327c90ecd06db410dde8802564f9a0
bsekisser <squirmyworms@embarqmail.com>
2024-09-02 23:18:35 -0400

    Rect: provision to initialize and load RectangleType and PointType.
    	PointType[Init, Load] added.
    	RectangleType[Init, Load] added.

--- source/Rect.c
+++ source/Rect.c
@@ -2,9 +2,37 @@
 
 /* **** */
 
+#include "ldst.h"
+
+/* **** */
+
 #include <string.h>
 
 /* **** */
+
+void PointTypeInit(PointType *const ptP)
+{
+	ptP->x = 0;
+	ptP->y = 0;
+}
+
+void* PointTypeLoad(PointType *const ptP, void* p)
+{
+	p = lds16be(&ptP->x, p);
+	return(lds16be(&ptP->y, p));
+}
+
+void RectangleTypeInit(RectangleType *const rP)
+{
+	PointTypeInit(&rP->topLeft);
+	PointTypeInit(&rP->extent);
+}
+
+void* RectangleTypeLoad(RectangleType *const rP, void* p)
+{
+	p = PointTypeLoad(&rP->topLeft, p);
+	return(PointTypeLoad(&rP->extent, p));
+}
 
 void RctCopyRectangle(const RectangleType* srcRectP, RectangleType* dstRectP)
 { memcpy(dstRectP, srcRectP, sizeof(RectangleType)); }
---
+++ source/include/xRect.h
@@ -0,0 +1,12 @@
+#pragma once
+
+/* **** */
+
+#include "sdk/include/Core/System/Rect.h"
+
+/* **** */
+
+void PointTypeInit(PointType *const ptP);
+void* PointTypeLoad(PointType *const ptP, void* p);
+void RectangleTypeInit(RectangleType *const rP);
+void* RectangleTypeLoad(RectangleType *const rP, void* p);
